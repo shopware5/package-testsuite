df433d0724acbb3a9207bb541fc013e1803d6cdb
Niklas Dzösch <n.dzoesch@shopware.com>
2017-06-28 10:04:17 +0200

    switched to pre-built images

--- docker/build-tools/wait-mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-retries=30
-while ! nc -z mysql 3306
-do
-    ((c++)) && ((c==retries)) && break
-    echo -n .
-    sleep 1;
-done
--- docker/build-tools/wait-smtp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-retries=30
-while ! nc -z smtp 1025
-do
-    ((c++)) && ((c==retries)) && break
-    echo -n .
-    sleep 1;
-done
