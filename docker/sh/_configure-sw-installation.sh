79cded07401ba82fc85a137acde29f19f288e04c
Soner Sayakci <s.sayakci@shopware.com>
2020-09-24 09:18:45 +0200

    NTR - Fix disabling cookie consent manager

--- docker/sh/_configure-sw-installation.sh
+++ docker/sh/_configure-sw-installation.sh
@@ -3,7 +3,7 @@
 echo "Setting extra config"
 
 # Disable first run wizard
-docker-compose run --rm tools bash -c 'cd /var/www/shopware && php bin/console sw:firstrunwizard:disable && php bin/console sw:cache:clear'
+docker-compose run --rm tools bash -c 'cd /var/www/shopware && php bin/console sw:firstrunwizard:disable'
 # Preset API key
 docker-compose run --rm tools mysql -u root -ptoor -h mysql -e 'UPDATE `shopware`.`s_core_auth` SET `apiKey`="8mnq6vav02p3buc8h2q4q6n137" WHERE `roleID`=1;'
 # Use custom testing config.php
@@ -17,7 +17,9 @@ docker-compose run --rm tools mysql -u root -ptoor -h mysql -e 'UPDATE `shopware
 # Use mocked update server
 docker-compose run --rm tools mysql -u root -ptoor -h mysql -e 'UPDATE `shopware`.`s_core_config_elements` SET `value`='"'"'s:23:"http://updates.example/";'"'"' WHERE `name`='"'"'update-api-endpoint'"'"';'
 # Disable Cookie Banner
-docker-compose run --rm tools mysql -u root -ptoor -h mysql -e 'UPDATE s_core_config_elements SET value = "b:0;" WHERE name = "show_cookie_note"'
+docker-compose run --rm tools mysql -u root -ptoor -h mysql shopware -e 'UPDATE s_core_config_elements SET value = "b:0;" WHERE name = "show_cookie_note"'
+
+docker-compose run --rm tools bash -c 'cd /var/www/shopware && php bin/console sw:cache:clear'
 
 echo "Chown directories to www-data"
 docker-compose run --rm tools chown -R www-data:www-data /var/www/shopware
