aa9a357b0f8e817735d16af88b4c7a89cccbcfe1
Soner Sayakci <s.sayakci@shopware.com>
2021-05-14 08:34:55 +0000

    Update stage_updater.sh

--- docker/stage_updater.sh
+++ docker/stage_updater.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-INSTALLER_URL='https://www.shopware.com/de/Download/redirect/version/sw5/file/install_5.6.0_3e81c54e1c57c6925e4d05336283ad18de9b10bb.zip'
+INSTALLER_URL='https://www.shopware.com/de/Download/redirect/version/sw5/file/install_5.6.10_b9471cf7c3f30dfc05d7c959f555c2a8d1c24420.zip'
 
 if [ "$1" = "" ]
 then
